821733efbb6ca96e681a5ed4055710ae09bca4aa
Daniel Landau <daniel@landau.fi>
2017-08-20 22:22:27 +0300

    Start work on new site
    * Switch to metalsmith site generation
    * Switch to styles built on Foundation

---
+++ build.js
@@ -0,0 +1,38 @@
+const Metalsmith = require('metalsmith')
+const markdown = require('metalsmith-markdown')
+const layouts = require('metalsmith-layouts')
+const assets = require('metalsmith-assets')
+const rootpath = require('metalsmith-rootpath')
+const debug = require('metalsmith-debug')
+const inPlace = require('metalsmith-in-place')
+const sass = require('metalsmith-sass')
+
+Metalsmith(__dirname)
+  .source('./src')
+  .destination('./_site')
+  .clean(true)
+  .use(rootpath())
+  .use(layouts({
+    engine: 'handlebars'
+  }))
+  .use(sass({
+    outputDir: 'css/',
+    includePaths: [ 'node_modules/foundation-sites/scss/' ]
+  }))
+  .use(inPlace())
+  .use(debug())
+  .use(assets({
+    source: './js',
+    destination: './js'
+  }))
+  .use(assets({
+    source: './img',
+    destination: './img'
+  }))
+  .use(assets({
+    source: './graphics',
+    destination: './graphics'
+  }))
+  .build(function(err, files) {
+    if (err) { throw err; }
+  });
